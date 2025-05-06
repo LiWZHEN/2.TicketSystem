b01788854b133a782b99a4c196dbc13b7c569716
livandan <livandan@sjtu.edu.cn>
2025-05-07 02:09:21 +0800

    finish the bpt

--- b_plus_tree_storage_test.cpp
+++ b_plus_tree_storage_test.cpp
@@ -1,18 +1,36 @@
-#include <fstream>
+#include <iostream>
+#include "b_plus_tree.h"
 
 int main() {
-  bool exist = false;
-  std::fstream file("data.txt");
-  if (file.is_open()) {
-    exist = true;
-  }
-  file.close();
-  if (!exist) {
-    std::ofstream new_file("data.txt");
-    new_file.close();
-  }
-  file.open("data.txt");
+  sjtu::bpt<int> bpt("map_file.txt", "data_file.txt");
 
-  file.close();
+  int n;
+  std::cin >> n;
+  for (int i = 0; i < n; ++i) {
+    std::string command;
+    std::cin >> command;
+    if (command == "insert") {
+      std::string index;
+      int value;
+      std::cin >> index >> value;
+      bpt.Insert(index, value);
+    } else if (command == "delete") {
+      std::string index;
+      int value;
+      std::cin >> index >> value;
+      bpt.Delete(index, value);
+    } else if (command == "find") {
+      std::string index;
+      std::cin >> index;
+      const auto ans = bpt.Find(index);
+      for (const auto it : ans) {
+        std::cout << it << ' ';
+      }
+      if (ans.empty()) {
+        std::cout << "null";
+      }
+      std::cout << '\n';
+    }
+  }
   return 0;
 }
