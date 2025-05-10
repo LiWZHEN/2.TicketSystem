e00654fab648851b0e028bdf0409dad2554c5b00
livandan <livandan@sjtu.edu.cn>
2025-05-10 22:10:38 +0800

    Allow fixed_str.h to perform default construction and copy construction. Tested.

--- test.cpp
+++ test.cpp
@@ -1,26 +1,55 @@
 #include <iostream>
 #include "fixed_Chinese.h"
-// #include "fixed_str.h"
+#include "fixed_str.h"
 
 int main() {
-  std::string str[5] = {"汉字串一", "这是站点名", "哇那这很好了", "浙江温州平阳", "上海闵行"};
+  std::string str("A123B_kaj_");
+  fixed_str<1, 20, allow_underline,underline_group_num> fixed1(str);
 
-  for (int i = 0; i < 5; ++i) {
-    fixed_Chinese<1, 5> Chinese(str[i]);
-
-    if (Chinese.is_valid()) {
-      std::cout << "Chinese valid\n";
-    } else {
-      std::cout << "Chinese invalid\n";
-    }
-
-    std::cout << "Length: " << Chinese.length() << '\n';
-
-    std::cout << "Print test ...\n";
-    Chinese.print();
-    std::cout << std::endl;
-
-    std::cout << Chinese[0] << std::endl;
+  if (fixed1.is_valid()) {
+    std::cout << "valid\n";
+  } else {
+    std::cout << "invalid\n";
   }
+
+  std::cout << "Length: " << fixed1.length() << '\n';
+
+  std::cout << "Print test ...\n";
+  fixed1.print();
+  std::cout << std::endl;
+
+  std::cout << fixed1[0] << std::endl;
+
+  fixed_str fixed2(fixed1);
+  fixed1 = "change1";
+  if (fixed1.is_valid()) {
+    std::cout << "valid\n";
+  } else {
+    std::cout << "invalid\n";
+  }
+
+  std::cout << "Length: " << fixed1.length() << '\n';
+
+  std::cout << "Print test ...\n";
+  fixed1.print();
+  std::cout << std::endl;
+
+  std::cout << fixed1[0] << std::endl;
+
+  fixed1 = fixed2;
+
+  if (fixed1.is_valid()) {
+    std::cout << "valid\n";
+  } else {
+    std::cout << "invalid\n";
+  }
+
+  std::cout << "Length: " << fixed1.length() << '\n';
+
+  std::cout << "Print test ...\n";
+  fixed1.print();
+  std::cout << std::endl;
+
+  std::cout << fixed1[0] << std::endl;
   return 0;
 }
