474490b1e809587726b9dc0e408f1a1197e4701f
livandan <livandan@sjtu.edu.cn>
2025-05-10 17:10:15 +0800

    Create a header file to handle English string.

--- fixed_str.h
+++ fixed_str.h
@@ -1,8 +1,60 @@
-//
-// Created by 20384 on 2025/5/10.
-//
-
 #ifndef FIXED_STR_H
 #define FIXED_STR_H
+
+template <int min_len, int max_len, char *special_valid, int special_valid_len>
+struct fixed_str {
+private:
+  char str[max_len];
+  int len = 0;
+  bool valid = true;
+public:
+  fixed_str(const std::string &string) {
+    if (string.length() < min_len || string.length() > max_len) { // the length is invalid
+      valid = false;
+    } else { // the length is valid
+      len = static_cast<int>(string.length());
+      for (int i = 0; i < len; ++i) {
+        str[i] = string[i];
+      }
+      if (special_valid_len != 0) {
+        for (int i = 0; i < len; ++i) {
+          if ((str[i] >= '0' && str[i] <= '9') || (str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
+            continue;
+          }
+          valid = false;
+          for (int j = 0; j < special_valid_len; ++j) {
+            if (str[i] == *(special_valid + i)) {
+              valid = true;
+              break;
+            }
+          }
+          if (!valid) {
+            break;
+          }
+        }
+      } else {
+        for (int i = 0; i < len; ++i) {
+          if ((str[i] >= '0' && str[i] <= '9') || (str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
+            continue;
+          }
+          valid = false;
+          break;
+        }
+      }
+    }
+  }
+
+  [[nodiscard]] int length() const {
+    return len;
+  }
+
+  [[nodiscard]] bool is_valid() const {
+    return valid;
+  }
+
+  char operator[](const int ind) const {
+    return str[ind];
+  }
+};
 
 #endif //FIXED_STR_H
